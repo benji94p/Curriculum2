3a25dbfa63c75608d72b8d678afc4642363ca332
Benjamin Pelzman <benjaminpelzman@MacBook-Pro-de-Benjamin.local>
2017-09-19 11:47:53 +0200

    burgermenuDone

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
 /* TRIGGER OVERLAY BURGER MENU */
 
 let hamburger_btn = document.querySelector('.hamburger_btn');
+let cross_btn = document.querySelector('.cross-btn');
+let modal = document.querySelector(".modal");
+cross_btn.addEventListener("click",closeBurger);
 hamburger_btn.addEventListener("click",toggleBurgerMenu);
 
 
 function toggleBurgerMenu () {
     console.log("burger menu clicked");
-    let modal = document.querySelector(".modal");
     if (modal.classList.contains('hide')) {
         modal.classList.remove('hide');
+        document.querySelector(".cross-btn").style.display ="block";
+        document.querySelector(".hamburger_btn").style.display ="none";
         }
-    else {
-         modal.classList.add('hide');
-    }
+
   }
+
+function closeBurger () {
+    console.log("burge closed");
+    document.querySelector(".cross-btn").style.display ="none";
+    modal.classList.add('hide');
+    document.querySelector(".hamburger_btn").style.display ="block";
+}
